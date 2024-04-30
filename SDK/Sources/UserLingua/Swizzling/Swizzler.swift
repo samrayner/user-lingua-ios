ac6e7d36fdd5aba447e621435eeac71117edb2bd
Sam Rayner <sam@samrayner.com>
2024-04-30 20:28:34 +0100

    Improve Swizzle safety

--- SDK/Sources/UserLingua/Swizzling/Swizzler.swift
+++ SDK/Sources/UserLingua/Swizzling/Swizzler.swift
@@ -12,18 +12,27 @@ protocol SwizzlerProtocol {
     func unswizzleForBackground()
 }
 
-struct Swizzler: SwizzlerProtocol {
+final class Swizzler: SwizzlerProtocol {
+    private var isSwizzledForForeground = false
+    private var isSwizzledForBackground = false
+
     func swizzleForForeground() {
+        guard !isSwizzledForForeground else { return }
+        defer { isSwizzledForForeground = true }
         UITraitCollection.swizzle()
         NotificationCenter.swizzle()
     }
 
     func unswizzleForForeground() {
+        guard isSwizzledForForeground else { return }
+        defer { isSwizzledForForeground = false }
         UITraitCollection.unswizzle()
         NotificationCenter.unswizzle()
     }
 
     func swizzleForBackground() {
+        guard !isSwizzledForBackground else { return }
+        defer { isSwizzledForBackground = true }
         UIView.swizzleAll()
         Bundle.swizzle()
         UILabel.swizzle()
@@ -31,6 +40,8 @@ struct Swizzler: SwizzlerProtocol {
     }
 
     func unswizzleForBackground() {
+        guard isSwizzledForBackground else { return }
+        defer { isSwizzledForBackground = false }
         UIView.unswizzleAll()
         Bundle.unswizzle()
         UILabel.unswizzle()
