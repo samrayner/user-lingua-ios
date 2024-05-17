79a5bbcdd284dac418423d3191410ffe07133384
Sam Rayner <sam@samrayner.com>
2024-05-17 13:32:14 +0100

    Add tooling

---
+++ Tools/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "tools",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/marmelroy/Zip", from: "2.1.2")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "update-libs",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Zip", package: "Zip")
+            ]
+        )
+    ]
+)
---
+++ Tools/Sources/update-libs/UpdateLibs.swift
@@ -0,0 +1,131 @@
+// UpdateLibs.swift
+
+import ArgumentParser
+import Foundation
+import Zip
+
+private let libraries: Set<Library> = [
+    .package(url: "https://github.com/pointfreeco/combine-schedulers", exact: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.10.4"),
+    .package(url: "https://github.com/pointfreeco/swift-case-paths", exact: "1.3.3"),
+    .package(url: "https://github.com/pointfreeco/swift-clocks", exact: "1.0.2"),
+    .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", exact: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", exact: "1.3.0"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", exact: "1.3.0"),
+    .package(url: "https://github.com/pointfreeco/swift-identified-collections", exact: "1.0.1"),
+    .package(url: "https://github.com/pointfreeco/swift-perception", exact: "1.1.7"),
+    .package(url: "https://github.com/pointfreeco/swiftui-navigation", exact: "1.3.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", exact: "1.1.0")
+//    .package(url: "https://github.com/klassen-software-solutions/KSSDiff", exact: "3.0.1")
+]
+
+private struct Library: Hashable {
+    let repoURL: URL
+    let version: String
+
+    static func package(
+        url: String,
+        exact version: String
+    ) -> Self {
+        .init(
+            repoURL: URL(string: url)!,
+            version: version
+        )
+    }
+
+    var zipURL: URL {
+        // URL(string: "\(repoURL)/archive/refs/tags/\(version).zip")!
+        URL(string: "\(repoURL)/archive/refs/heads/main.zip")!
+    }
+}
+
+@main
+struct UpdateLibs: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(abstract: "Download, move and rename SDK dependencies.")
+
+    var fileManager: FileManager { FileManager.default }
+
+    private func downloadLibrary(_ library: Library) async throws -> URL {
+        let (data, _) = try await URLSession.shared.data(from: library.zipURL)
+
+        let tempDir = NSURL.fileURL(withPath: NSTemporaryDirectory(), isDirectory: true)
+
+        let zipFile = tempDir.appendingPathComponent("\(library.repoURL.lastPathComponent).zip")
+
+        try data.write(to: zipFile)
+
+        let unzippedDir = tempDir.appendingPathComponent("\(library.repoURL.lastPathComponent)-\(library.version)")
+
+        try Zip.unzipFile(
+            zipFile,
+            destination: unzippedDir,
+            overwrite: true,
+            password: nil
+        )
+
+        return unzippedDir
+    }
+
+    private func targetDirs(of unzippedDir: URL) throws -> [URL] {
+        let enumerator = fileManager.enumerator(at: unzippedDir, includingPropertiesForKeys: nil)!
+
+        var targetDirs: [URL] = []
+
+        for case let url as URL in enumerator where enumerator.level < 4 {
+            if url.hasDirectoryPath,
+               url.deletingLastPathComponent().lastPathComponent == "Sources",
+               !url.lastPathComponent.hasSuffix("benchmark") {
+                targetDirs.append(url)
+            }
+        }
+
+        return targetDirs
+    }
+
+    mutating func run() async throws {
+        let fileManager = FileManager.default
+
+        var unzippedDirs: [URL] = []
+        for library in libraries {
+            try await unzippedDirs.append(downloadLibrary(library))
+        }
+
+        let currentDir = URL(fileURLWithPath: fileManager.currentDirectoryPath)
+
+        let sourceDirs = try unzippedDirs
+            .flatMap(targetDirs)
+            .filter {
+                ![
+                    "DependenciesMacros",
+                    "DependenciesMacrosPlugin",
+                    "DependenciesTestObserver"
+                ].contains($0.lastPathComponent)
+            }
+
+        for source in sourceDirs {
+            let targetName = source.lastPathComponent
+
+            try? fileManager.removeItem(at: source.appendingPathComponent("Documentation.docc"))
+
+//            let fileURLs = fileManager.enumerator(at: source, includingPropertiesForKeys: nil)!
+//                .compactMap { $0 as? URL }
+//                .filter { !$0.hasDirectoryPath && $0.pathExtension == "swift" }
+//
+//            for url in fileURLs {
+//                let encoding = String.Encoding.utf8
+//                var contents = try String(contentsOf: url, encoding: encoding)
+//
+//                //modify contents as needed
+//
+//                try contents.write(to: url, atomically: false, encoding: encoding)
+//            }
+
+            let destination = currentDir.appendingPathComponent("../SDK/Sources/\(targetName)")
+            try? fileManager.removeItem(at: destination)
+            try fileManager.copyItem(
+                at: source,
+                to: destination
+            )
+        }
+    }
+}
