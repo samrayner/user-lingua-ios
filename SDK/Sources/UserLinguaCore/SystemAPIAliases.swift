5cfb3f3cac051bd45a89aa574121479a869ec23c
Sam Rayner <sam@samrayner.com>
2024-07-04 16:37:45 +0100

    Make SystemAPIAliases package scoped

--- SDK/Sources/UserLinguaCore/SystemAPIAliases.swift
+++ SDK/Sources/UserLinguaCore/SystemAPIAliases.swift
@@ -3,18 +3,18 @@
 import Foundation
 import SwiftUI
 
-public enum SystemString {
-    public static let initLocalizedOptionsTableBundleLocaleComment = String.init(localized:options:table:bundle:locale:comment:)
-    public static let initLocalizedTableBundleLocaleComment = String.init(localized:table:bundle:locale:comment:)
-    public static let initLocalizedDefaultValueOptionsTableBundleLocaleComment = String
+package enum SystemString {
+    package static let initLocalizedOptionsTableBundleLocaleComment = String.init(localized:options:table:bundle:locale:comment:)
+    package static let initLocalizedTableBundleLocaleComment = String.init(localized:table:bundle:locale:comment:)
+    package static let initLocalizedDefaultValueOptionsTableBundleLocaleComment = String
         .init(localized:defaultValue:options:table:bundle:locale:comment:)
-    public static let initLocalizedDefaultValueTableBundleLocaleComment = String.init(localized:defaultValue:table:bundle:locale:comment:)
-    public static let initFormatLocaleArguments = String.init(format:locale:arguments:)
-    public static let initFormatArguments = String.init(format:arguments:)
+    package static let initLocalizedDefaultValueTableBundleLocaleComment = String.init(localized:defaultValue:table:bundle:locale:comment:)
+    package static let initFormatLocaleArguments = String.init(format:locale:arguments:)
+    package static let initFormatArguments = String.init(format:arguments:)
 }
 
-public enum SystemText {
-    public static let initTableNameBundleComment = Text.init(_:tableName:bundle:comment:)
-    public static let initVerbatim: (String) -> Text = Text.init(verbatim:)
-    public static let initLocalizedStringResource: (LocalizedStringResource) -> Text = Text.init(_:)
+package enum SystemText {
+    package static let initTableNameBundleComment = Text.init(_:tableName:bundle:comment:)
+    package static let initVerbatim: (String) -> Text = Text.init(verbatim:)
+    package static let initLocalizedStringResource: (LocalizedStringResource) -> Text = Text.init(_:)
 }
